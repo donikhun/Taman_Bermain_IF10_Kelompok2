e5124285cb3a223c8a254cc277316c56e665f3ba
Febrian Doni <donikhun@gmail.com>
2012-06-19 01:21:16 +0700

    tambah bouncy castle

--- main.cpp
+++ main.cpp
@@ -429,6 +429,224 @@ void gazibu(){
     glutSolidSphere(0.4, 80, 120);
     glPopMatrix();
 }
+
+//bouncy castle
+void bouncyCastle(){
+    //sisi kanan
+        
+    
+        glColor3f(1, 0.58,0);
+        glPushMatrix();
+        glScaled(5.80, 1.0, 5.80);
+        glTranslatef(1,0.5,0);
+        glutSolidCube(2);
+        glPopMatrix();
+        glPushMatrix();;
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glTranslatef(0.0f, 1.0f, 0.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glTranslatef(0.0f, 2.0f, 0.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glTranslatef(0.0f, 3.0f, 0.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glTranslatef(0.0f, 4.0f, 0.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glTranslatef(0.0f, 5.0f, 0.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        
+        //sisi belakang
+        glPushMatrix();
+        glRotatef(90,0.0,1.0,0.0);
+        glTranslatef(-6.0f, 0.0f, 6.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glRotatef(90,0.0,1.0,0.0);
+        glTranslatef(-6.0f, 1.0f, 6.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glRotatef(90,0.0,1.0,0.0);
+        glTranslatef(-6.0f, 2.0f, 6.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();     
+        glPushMatrix();
+        glRotatef(90,0.0,1.0,0.0);
+        glTranslatef(-6.0f, 3.0f, 6.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();      
+        glPushMatrix();
+        glRotatef(90,0.0,1.0,0.0);
+        glTranslatef(-6.0f, 4.0f, 6.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glRotatef(90,0.0,1.0,0.0);
+        glTranslatef(-6.0f, 5.0f, 6.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        
+        //sisi kiri
+        glPushMatrix();
+        glTranslatef(12.0f, 0.0f, 0.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glTranslatef(12.0f, 1.0f, 0.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glTranslatef(12.0f, 2.0f, 0.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glTranslatef(12.0f, 3.0f, 0.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glTranslatef(12.0f, 4.0f, 0.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glTranslatef(12.0f, 5.0f, 0.0f);
+        glScaled(1.0, 1.0, 15.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        
+        //tiang 1
+        glColor3f(0.8,0.2,0.3);
+        glPushMatrix();
+        glRotatef(90,1,0,0);
+        glTranslatef(12.0f, 6.0f, -3.5f);
+        glScaled(1.4, 1.4, 11.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        
+        //tiang 2
+        glPushMatrix();
+        glRotatef(90,1,0,0);
+        glTranslatef(0.0f, 6.0f, -3.5f);
+        glScaled(1.4, 1.4, 11.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        
+       //tiang 3
+        glPushMatrix();
+        glRotatef(90,1,0,0);
+        glTranslatef(0.0f, -6.0f, -3.5f);
+        glScaled(1.4, 1.4, 11.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        
+        //tiang 4
+        glPushMatrix();
+        glRotatef(90,1,0,0);
+        glTranslatef(12.0f, -6.0f, -3.5f);
+        glScaled(1.4, 1.4, 11.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        
+        //tangga
+        glColor3f(0.2,0.3,1);
+        glPushMatrix();
+        glRotatef(90,0.0,1.0,0.0);
+        glTranslatef(6.0f, 0.6f, 6.0f);
+        glScaled(1.0, 1.0, 13.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glRotatef(90,0.0,1.0,0.0);
+        glTranslatef(7.0f, 0.3f, 6.0f);
+        glScaled(1.0, 1.0, 13.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glRotatef(90,0.0,1.0,0.0);
+        glTranslatef(8.0f, 0.0f, 6.0f);
+        glScaled(1.0, 1.0, 13.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        
+        //gate
+        glColor3f(0.8,0.2,0.3);
+        glPushMatrix();
+        glRotatef(90,0.0,1.0,0.0);
+        glRotatef(15,1.0,0.0,0.0);
+        glTranslatef(6.0f, 10.0f, 6.0f);
+        glScaled(1.0, 1.0, 8.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glRotatef(90,0.0,1.0,0.0);
+        glRotatef(-15,1.0,0.0,0.0);
+        glTranslatef(6.0f, 7.0f, 5.0f);
+        glScaled(1.0, 1.0, 8.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        glPushMatrix();
+        glRotatef(90,0.0,1.0,0.0);
+        glRotatef(90,1.0,0.0,0.0);
+        glTranslatef(6.0f, 5.8f, -9.0f);
+        glScaled(1.2, 1.2, 5.0);
+        glutSolidTorus(0.4,0.4,60,80);
+        glPopMatrix();
+        
+        //kerucut
+        glColor3f(0.55,0.34,0.114);
+        glPushMatrix();
+        glRotatef(-90,1.0,0.0,0.0);
+        glTranslatef(5.8f, 6.0f, 10.8f);
+        glutSolidCone(0.7,1.5,10,10);
+        glPopMatrix();
+        glPushMatrix();
+        glRotatef(-90,1.0,0.0,0.0);
+        glTranslatef(12.0f, 6.0f, 7.8f);
+        glutSolidCone(0.7,1.5,10,10);
+        glPopMatrix();
+        glPushMatrix();
+        glRotatef(-90,1.0,0.0,0.0);
+        glTranslatef(12.0f, -6.0f, 7.8f);
+        glutSolidCone(0.7,1.5,10,10);
+        glPopMatrix();
+        glPushMatrix();
+        glRotatef(-90,1.0,0.0,0.0);
+        glTranslatef(0.0f, 6.0f, 7.8f);
+        glutSolidCone(0.7,1.5,10,10);
+        glPopMatrix();
+        glPushMatrix();
+        glRotatef(-90,1.0,0.0,0.0);
+        glTranslatef(0.0f, -6.0f, 7.8f);
+        glutSolidCone(0.7,1.5,10,10);
+        glPopMatrix();
+}
 //Loads a terrain from a heightmap.  The heights of the terrain range from
 //-height / 2 to height / 2.
 
@@ -617,10 +835,15 @@ void drawScene() {
     glScalef(2,2,2);
     glRotatef(90, 1, 0, 0);
     glTranslatef(15.0,1.5,10.0);
-     //lantai
     gazibu();
     glPopMatrix();
     
+    glPushMatrix();
+    glScalef(0.5,0.5,0.5);
+    glRotatef(90, 1, 0, 0);
+    glTranslatef(0.0,1.5,10.0);
+    bouncyCastle();
+    glPopMatrix();
 
     ///*
     //terrain beruang
@@ -669,7 +892,7 @@ int main(int argc, char** argv) {
     glutDisplayFunc(drawScene);
     glutKeyboardFunc(handleKeypress);
     glutReshapeFunc(handleResize);
-    //glutTimerFunc(25, update, 0);
+    glutTimerFunc(25, update, 0);
 
     glutMainLoop();
     return 0;
